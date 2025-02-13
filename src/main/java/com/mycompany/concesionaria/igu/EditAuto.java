d8975dcb297a5e8d5683bb0ea2480d6b9e5a5af7
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2025-02-13 12:07:03 +0100

    EditAuto frame

---
+++ src/main/java/com/mycompany/concesionaria/igu/EditAuto.java
@@ -0,0 +1,334 @@
+package com.mycompany.concesionaria.igu;
+
+import com.mycompany.concesionaria.logica.Automovil;
+import com.mycompany.concesionaria.logica.Controladora;
+
+/**
+ *
+ * @author Javi
+ */
+public class EditAuto extends javax.swing.JFrame {
+
+    Controladora controladora;
+    DataView parentFrame;
+    Automovil auto;
+
+    public EditAuto(Controladora controladora, DataView parentFrame, Automovil auto) {
+        initComponents();
+
+        this.controladora = controladora;
+        this.parentFrame = parentFrame;
+        this.auto = auto;
+
+        txtMarca.setText(auto.getBrand());
+        txtModelo.setText(auto.getModel());
+        txtColor.setText(auto.getColor());
+        txtPlaca.setText(auto.getPlate());
+        txtPlaca.setEnabled(false);
+
+        setTitle("Edit Auto Form");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        pPrincipal = new javax.swing.JPanel();
+        pTitle = new javax.swing.JPanel();
+        lTitle = new javax.swing.JLabel();
+        pActionBtns = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        lMarca = new javax.swing.JLabel();
+        txtMarca = new javax.swing.JTextField();
+        lModelo = new javax.swing.JLabel();
+        txtModelo = new javax.swing.JTextField();
+        lColor = new javax.swing.JLabel();
+        txtColor = new javax.swing.JTextField();
+        lPlaca = new javax.swing.JLabel();
+        txtPlaca = new javax.swing.JTextField();
+        lPuertas = new javax.swing.JLabel();
+        txtPuertas = new javax.swing.JTextField();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        btnEdit = new javax.swing.JButton();
+        jPanel6 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pPrincipal.setLayout(new java.awt.BorderLayout());
+
+        pTitle.setLayout(new java.awt.GridBagLayout());
+
+        lTitle.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        lTitle.setText("CONCESIONARIA");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(28, 204, 32, 200);
+        pTitle.add(lTitle, gridBagConstraints);
+
+        pPrincipal.add(pTitle, java.awt.BorderLayout.PAGE_START);
+
+        pActionBtns.setLayout(new java.awt.BorderLayout());
+
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        lMarca.setText("Marca* :");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        jPanel1.add(lMarca, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        jPanel1.add(txtMarca, gridBagConstraints);
+
+        lModelo.setText("Modelo* :");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        jPanel1.add(lModelo, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        jPanel1.add(txtModelo, gridBagConstraints);
+
+        lColor.setText("Color* :");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        jPanel1.add(lColor, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        jPanel1.add(txtColor, gridBagConstraints);
+
+        lPlaca.setText("Placa* :");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        jPanel1.add(lPlaca, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        jPanel1.add(txtPlaca, gridBagConstraints);
+
+        lPuertas.setText("Puertas* :");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        jPanel1.add(lPuertas, gridBagConstraints);
+
+        txtPuertas.setEnabled(false);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        jPanel1.add(txtPuertas, gridBagConstraints);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 5;
+        jPanel1.add(jPanel3, gridBagConstraints);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 5;
+        jPanel1.add(jPanel4, gridBagConstraints);
+
+        pActionBtns.add(jPanel1, java.awt.BorderLayout.PAGE_START);
+
+        jPanel2.setLayout(new java.awt.GridBagLayout());
+
+        btnEdit.setText("EDIT");
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.ipadx = 10;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(20, 0, 20, 0);
+        jPanel2.add(btnEdit, gridBagConstraints);
+
+        pActionBtns.add(jPanel2, java.awt.BorderLayout.PAGE_END);
+
+        pPrincipal.add(pActionBtns, java.awt.BorderLayout.CENTER);
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+
+        pPrincipal.add(jPanel6, java.awt.BorderLayout.PAGE_END);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+        if (isFormValid()) {
+            editAuto();
+        } else {
+            NotificationHandle.showWarningDialog(pPrincipal, Message.FALTAN_DATOS);
+        }
+    }//GEN-LAST:event_btnEditActionPerformed
+
+    private void editAuto() {
+        Automovil formData = getFormData();
+
+        if (dataChanges()) {
+            int confirmation = NotificationHandle.showConfirmationDialog(pPrincipal, Message.CONFIRMAR_EDITAR);
+
+            if (confirmation == 0) {
+                auto.setBrand(formData.getBrand());
+                auto.setModel(formData.getModel());
+                auto.setColor(formData.getColor());
+
+                try {
+                    controladora.editAutomovil(auto);
+                    parentFrame.initTable();
+
+                    NotificationHandle.showInfoDialog(pPrincipal, Message.COCHE_EDITADO);
+                    dispose();
+                } catch (Exception e) {
+                    NotificationHandle.showErrorDialog(pPrincipal, Message.ERROR_CONEXION);
+                }
+            }
+
+        } else {
+            NotificationHandle.showWarningDialog(pPrincipal, Message.MISMOS_DATOS);
+        }
+    }
+
+    private Automovil getFormData() {
+        String marca = txtMarca.getText().trim();
+        String modelo = txtModelo.getText().trim();
+        String color = txtColor.getText().trim();
+        String placa = txtPlaca.getText().trim();
+
+        Automovil autoData = new Automovil(marca, modelo, color, placa);
+
+        return autoData;
+    }
+
+    private boolean isFormValid() {
+        Automovil autoData = getFormData();
+
+        return !autoData.getBrand().isEmpty() && !autoData.getModel().isEmpty() && !autoData.getColor().isEmpty() && !autoData.getPlate().isEmpty();
+    }
+
+    private boolean dataChanges() {
+        Automovil formData = getFormData();
+
+        return !auto.getBrand().equals(formData.getBrand()) || !auto.getModel().equals(formData.getModel()) || !auto.getColor().equals(formData.getColor());
+    }
+
+    @Override
+    public void dispose() {
+        if (parentFrame != null) {
+            parentFrame.setVisible(true);
+        }
+
+        super.dispose();
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnEdit;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JLabel lColor;
+    private javax.swing.JLabel lMarca;
+    private javax.swing.JLabel lModelo;
+    private javax.swing.JLabel lPlaca;
+    private javax.swing.JLabel lPuertas;
+    private javax.swing.JLabel lTitle;
+    private javax.swing.JPanel pActionBtns;
+    private javax.swing.JPanel pPrincipal;
+    private javax.swing.JPanel pTitle;
+    private javax.swing.JTextField txtColor;
+    private javax.swing.JTextField txtMarca;
+    private javax.swing.JTextField txtModelo;
+    private javax.swing.JTextField txtPlaca;
+    private javax.swing.JTextField txtPuertas;
+    // End of variables declaration//GEN-END:variables
+}
