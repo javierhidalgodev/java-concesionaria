bdc34fb6d7cdccc83889462592b0d74286b4713c
javierhidalgodev <javierhidalgodev@gmail.com>
2025-02-13 20:27:47 +0100

    FormCreate interface

---
+++ src/main/java/com/mycompany/concesionaria/igu/FormCreate.java
@@ -0,0 +1,14 @@
+package com.mycompany.concesionaria.igu;
+
+import com.mycompany.concesionaria.logica.Automovil;
+
+/**
+ *
+ * @author Javi
+ */
+public interface FormCreate {
+
+    public Automovil getData();
+    public boolean isValidForm();
+    
+}
